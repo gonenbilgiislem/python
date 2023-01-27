cd7746490b8a0e4f2b7b9d5746963ec3b3a8d0a1
baris.ozen <bilgiislem@balikesirgonen.bel.tr>
2023-01-27 19:48:28 +0300

    .

---
+++ matematikModule.py
@@ -0,0 +1,15 @@
+def topla(sayi1,sayi2):
+    return sayi1+sayi2
+def cikar(sayi1,sayi2):
+    return sayi1-sayi2
+def carp(sayi1,sayi2):
+    return sayi1*sayi2
+def bol(sayi1,sayi2):
+    return sayi1/sayi2
+
+
+customer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+}
---
+++ matematiktest.py
@@ -0,0 +1,12 @@
+import matematikModule as mm
+print(f'toplaaa : {mm.topla(2,3)}')
+print(mm.bol(2,3))
+print(mm.carp(2,3))
+print(mm.cikar(2,3))
+#%%
+print(mm.customer["name"])
+print(mm.customer["age"])
+print(mm.customer["is_verified"])
+#%%
+from matematikModule import topla
+print(topla(2,3))
