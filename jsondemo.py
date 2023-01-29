1b0af83bdfd7994e1564e887ff97a153703349d3
Barış Özen <bilgiislem@balikesirgonen.bel.tr>
2023-01-29 22:03:16 +0300

    Zenbook 20.01.2023

---
+++ jsondemo.py
@@ -0,0 +1,20 @@
+import json
+
+with open('users.json') as users:
+    data = json.load(users)
+    # print(data)
+    # for user in data:
+    #     print("Adı : " + user['name'])
+    #     print("Soyadı : " + user['email'])
+    #     print(user['address']['street'])
+    #     print(f'Aparman adı : {user["address"]["suite"]}, Şehri : {user["address"]["city"]}')
+    #     print("---------------------------------")
+    # print(data[0]['name'])
+    # print(data[0]['email'])
+    # print(data[0]['address']['street'])
+
+for x in range(len(data)):
+    print(data[x]['name'])
+    print(data[x]['email'])
+    print(data[x]['address']['street'])
+    print("---------------------------------")
--- jsonss.py
+++ jsonss.py
@@ -1,5 +1,7 @@
 import json
 
+import jsons
+
 data = '{"name": "John", "age": 30, "city": "New York"}'
 y = json.loads(data)
 print(y["age"])
@@ -8,35 +10,9 @@ print(y["city"])
 
 customers = {
     "firstName": "John",
-    "lastName": "Doe",
-    "age": 30,
-    "address": {
-        "streetAddress": "21 2nd Street",
-        "city": "New York",
-        "state": "NY",
-        "postalCode": "10021"
-    },
-    "phoneNumber": [
-        {
-            "type": "home",
-            "number": "212 555-1234"
-        },
-        {
-            "type": "fax",
-            "number": "646 555-4567"
-        }
-    ]
+    "lastName": "Doe"
 }
 
-print(customers["firstName"])
-print(customers["lastName"])
-print(customers["age"])
-print(customers["address"]["streetAddress"])
-print(customers["address"]["city"])
-print(customers["address"]["state"])
-print(customers["address"]["postalCode"])
-print(customers["phoneNumber"][0]["type"])
-print(customers["phoneNumber"][0]["number"])
-print(customers["phoneNumber"][1]["type"])
-print(customers["phoneNumber"][1]["number"])
-
+customerjson = json.dumps(customers)
+print(customerjson)
+print(customers)
